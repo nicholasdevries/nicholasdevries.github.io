1ae95084adef7b34fa4f5d9feff2d39710decaa1
Nicholas DeVries <iamnicholasdevries@gmail.com>
2023-09-22 04:47:04 +0000

    feat: replaced background styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+import "../tailwind.css";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import React from "react";
 import colors from "tailwindcss/colors";
-import { BannerImage } from "../components/BannerImage";
-import "../styles/globals.css";
 import { clsx } from "../lib/clsx";
+import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,13 +26,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           "bg-white text-black antialiased dark:bg-gray-800 dark:text-white",
         )}
       >
-        <div className={"absolute inset-0 opacity-5 blur-3xl dark:opacity-10"} aria-hidden="true">
-          <BannerImage className="h-full w-full object-cover transition-opacity duration-1000" />
+        <div className="absolute inset-0">
+          <div className={clsx(styles.glow, "absolute inset-0")} />
+          <div className={clsx(styles.pattern, "absolute inset-0")} />
         </div>
-        <div
-          className="bg-grid-light dark:bg-grid-dark absolute inset-0 h-full w-full bg-center"
-          aria-hidden="true"
-        />
         <main className="relative flex min-h-screen items-center justify-center">{children}</main>
       </body>
     </html>
