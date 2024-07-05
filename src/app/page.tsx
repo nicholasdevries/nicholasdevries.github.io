5009804f9956dd53d64a82bbcc76bc6f385e802d
Nicholas DeVries <iamnicholasdevries@gmail.com>
2024-07-05 20:06:42 +0000

    chore: deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import { ProfileImage } from "../components/ProfileImage";
 export default function Home() {
   return (
     <div className="flex flex-col items-center space-y-8 px-[10vw] py-[10vh] md:flex-row md:space-x-8 md:space-y-0 lg:space-x-12">
-      <div className="w-36 max-w-full shrink-0 overflow-hidden rounded-full bg-black/5 drop-shadow-2xl dark:bg-white/5 md:w-48 lg:w-56">
+      <div className="w-36 max-w-full shrink-0 overflow-hidden rounded-full bg-black/5 drop-shadow-2xl md:w-48 lg:w-56 dark:bg-white/5">
         <ProfileImage />
       </div>
       <div className="text-center md:text-left">
         <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-6xl">
           Nicholas DeVries
         </h1>
-        <p className="text-lg tracking-tight text-black/60 dark:text-white/60 md:text-xl lg:text-2xl">
+        <p className="text-lg tracking-tight text-black/60 md:text-xl lg:text-2xl dark:text-white/60">
           Staff Software Engineer at{" "}
           <a
             href="https://peak6.com/"
