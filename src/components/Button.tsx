836d05a0ca6bf34bb54ade5ca623ebcc5a782e81
Nicholas DeVries <iamnicholasdevries@gmail.com>
2025-04-08 22:47:46 -0500

    tailwind v4 upgrade

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ export const Button = <T extends React.ElementType = 'button'>({
   return (
     <Component
       className={clsx(
-        'relative inline-flex h-10 cursor-pointer flex-nowrap items-center justify-center rounded bg-gray-100/50 px-4 font-semibold text-black outline-none ring-blue-500 ring-offset-2 ring-offset-white drop-shadow transition-colors hover:bg-gray-100/75 focus-visible:ring-2 dark:bg-gray-900/50 dark:text-white dark:ring-blue-400 dark:ring-offset-gray-800 dark:hover:bg-gray-900/75',
+        'relative inline-flex h-10 cursor-pointer flex-nowrap items-center justify-center rounded-sm bg-gray-100/50 px-4 font-semibold text-black outline-hidden ring-blue-500 ring-offset-2 ring-offset-white drop-shadow-sm transition-colors hover:bg-gray-100/75 focus-visible:ring-2 dark:bg-gray-900/50 dark:text-white dark:ring-blue-400 dark:ring-offset-gray-800 dark:hover:bg-gray-900/75',
         props.className,
       )}
       {...props}
