a9f76fc1fb18dd13321fef7ab113cdcacfaaab4e
Nicholas DeVries <iamnicholasdevries@gmail.com>
2025-06-18 10:33:27 -0500

    upgrading Biome

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import NextImage from 'next/image'
 import type { ImageProps } from 'next/image'
+import NextImage from 'next/image'
 
 export const Image = (props: Omit<ImageProps, 'loader'>) => (
   <NextImage {...props} loader={({ src }) => src} />
--- src/lib/clsx.ts
+++ src/lib/clsx.ts
@@ -1,5 +1,5 @@
-import classnames from 'clsx'
 import type { ClassValue } from 'clsx'
+import classnames from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export const clsx = (...classes: Array<ClassValue>) => twMerge(classnames(...classes))
